787e891c73c9ce5bf0a1749d1a50ca95a8a2d0e9
karan soni <karanparekh1011@gmail.com>
2025-06-27 22:05:36 +0530

    Add files via upload

---
+++ Indian Clock/src/App.js
@@ -0,0 +1,15 @@
+import Heading from "./components/Heading";
+import Slogan from "./components/Slogan";
+import Time from "./components/Time";
+import "./App.css";
+function App() {
+  return (
+    <center className="container">
+      <Heading />
+      <Slogan />
+      <Time />
+    </center>
+  );
+}
+
+export default App;
